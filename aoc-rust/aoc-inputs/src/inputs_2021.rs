0384c9756e8efbfd58dafc949dccb0730981cbb6
Pi Delport <pjdelport@gmail.com>
2021-12-10 20:04:40 +0200

    chore(rust,2021): add day 3 inputs

--- aoc-rust/aoc-inputs/src/inputs_2021.rs
+++ aoc-rust/aoc-inputs/src/inputs_2021.rs
@@ -7,3 +7,8 @@ pub mod day_2 {
     pub const EXAMPLE: &str = include_str!("../../../aoc-input-files/2021/day-2/example.txt");
     pub const INPUT: &str = include_str!("../../../aoc-input-files/2021/day-2/input.txt");
 }
+
+pub mod day_3 {
+    pub const EXAMPLE: &str = include_str!("../../../aoc-input-files/2021/day-3/example.txt");
+    pub const INPUT: &str = include_str!("../../../aoc-input-files/2021/day-3/input.txt");
+}
