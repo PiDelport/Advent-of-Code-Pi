59b89e69e8bbf5039d6a0c69940d38687cb48154
Pi Delport <pjdelport@gmail.com>
2021-12-14 20:12:05 +0200

    feat(rust,2021): add day 3 part 2

--- aoc-rust/aoc-2021/src/day_3_binary_diagnostic.rs
+++ aoc-rust/aoc-2021/src/day_3_binary_diagnostic.rs
@@ -180,7 +180,78 @@ pub mod part_1 {
 }
 
 pub mod part_2 {
-    // use super::input::*;
+    //! For part 2
+
+    use std::cmp::Ordering;
+
+    use anyhow::anyhow;
+    use bitvec::prelude::{BitBox, BitSlice};
+
+    pub type OxygenGeneratorRating = u64;
+    pub type Co2ScrubberRating = u64;
+
+    /// Partition entries by the value at the indexed bit.
+    ///
+    /// # Panics
+    ///
+    /// If `index` is out of range for any entry.
+    fn partition_by_bit(entries: Vec<BitBox>, index: usize) -> (Vec<BitBox>, Vec<BitBox>) {
+        // Partition entries by their value at index.
+        let (with_one, with_zero): (Vec<BitBox>, Vec<BitBox>) = entries
+            .into_iter()
+            .partition(|bits| *bits.get(index).as_deref().unwrap());
+
+        use Ordering::*;
+        match Ord::cmp(&with_one.len(), &with_zero.len()) {
+            Less => (with_zero, with_one),
+            Equal => (with_one, with_zero),
+            Greater => (with_one, with_zero),
+        }
+    }
+
+    /// Recursively find a particular rating.
+    ///
+    /// This assumes bits before `index` have been checked appropriately.
+    fn find_rating(
+        candidates: Vec<BitBox>,
+        index: usize,
+        most_common: bool,
+    ) -> OxygenGeneratorRating {
+        match candidates.len() {
+            0 => panic!("determine_rating called with empty input"),
+            1 => {
+                let bits = candidates.get(0).unwrap();
+                from_bits(bits).unwrap()
+            }
+            _more => {
+                let (o2s, co2s) = partition_by_bit(candidates, index);
+                let next = if most_common { o2s } else { co2s };
+                find_rating(next, index + 1, most_common)
+            }
+        }
+    }
+
+    pub fn determine_ratings(entries: Vec<BitBox>) -> (OxygenGeneratorRating, Co2ScrubberRating) {
+        let (o2s, co2s) = partition_by_bit(entries, 0);
+        let oxygen_generator_rating = find_rating(o2s, 1, true);
+        let co2_scrubber_rating = find_rating(co2s, 1, false);
+        (oxygen_generator_rating, co2_scrubber_rating)
+    }
+
+    /// Convert bits back to an integer.
+    ///
+    /// XXX: Do something less terrible here.
+    fn from_bits(bits: &BitSlice) -> anyhow::Result<u64> {
+        let init: anyhow::Result<u64> = Ok(0);
+        bits.iter().by_val().fold(init, |acc, bit| {
+            let acc = acc?;
+            let bit = bit as u64;
+            let acc = acc
+                .checked_shl(1)
+                .ok_or_else(|| anyhow!("unexpected overflow: {acc}"))?;
+            Ok(acc + bit)
+        })
+    }
 }
 
 #[cfg(test)]
@@ -191,15 +262,16 @@ mod tests {
 
     use super::input::*;
     use super::part_1::*;
-    // use super::part_2::*;
+    use super::part_2::*;
 
     pub fn part_1(report: &[BitBox]) -> u64 {
         let (gamma, epsilon) = decode_report(report).unwrap();
-        u64::checked_mul(gamma, epsilon).unwrap_or_else(|| panic!("overflow: {gamma} * {epsilon}"))
+        safe_mul(gamma, epsilon)
     }
 
-    fn part_2(_: &[BitBox]) -> u32 {
-        todo!()
+    fn part_2(report: Vec<BitBox>) -> u64 {
+        let (oxygen_generator_rating, co2_scrubber_rating) = determine_ratings(report);
+        safe_mul(oxygen_generator_rating, co2_scrubber_rating)
     }
 
     #[test]
@@ -209,14 +281,17 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part_2() {
-        assert_eq!(part_2(&unwrap_bits(parse_lines(EXAMPLE))), 230);
-        assert_eq!(part_2(&unwrap_bits(parse_lines(INPUT))), 0);
+        assert_eq!(part_2(unwrap_bits(parse_lines(EXAMPLE))), 230);
+        assert_eq!(part_2(unwrap_bits(parse_lines(INPUT))), 3385170);
     }
 
     /// XXX: Unwrap the [`ParseBits`] newtype.
     fn unwrap_bits(report: Vec<ParseBits>) -> Vec<BitBox> {
         report.into_iter().map(|ParseBits(bits)| bits).collect()
+    }
+
+    fn safe_mul(a: u64, b: u64) -> u64 {
+        u64::checked_mul(a, b).unwrap_or_else(|| panic!("overflow: {a} * {b}"))
     }
 }
